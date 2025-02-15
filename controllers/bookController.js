a362e1143718e20c1bdcde6b9f31045c3601349a
sobhan <sobhan.ziaee73@gmail.com>
2025-02-15 21:58:21 +0330

    create get book by id

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,15 +1,34 @@
-const url = require("url");
-
 const bookModel = require("./../models/Book");
 
 const getAll = async (req, res) => {
     const books = await bookModel.find();
-
     res.writeHead(200, { "Content-Type": "application/json" });
     res.write(JSON.stringify(books));
     res.end();
 }
 
-module.exports = {
-    getAll,
+const getById = async (req, res, id) => {  // ✅ دریافت id به‌صورت مستقیم
+    if (!id) {
+        res.writeHead(400, { "Content-Type": "application/json" });
+        res.write(JSON.stringify({ message: "لطفاً یک شناسه معتبر ارسال کنید" }));
+        res.end();
+        return;
+    }
+
+    const books = await bookModel.find();
+    const book = books.find((book) => book.id == id);  // ✅ مقایسه انعطاف‌پذیر
+
+    if (book) {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.write(JSON.stringify(book));
+        res.end();
+    } else {
+        res.writeHead(404, { "Content-Type": "application/json" });
+        res.write(JSON.stringify({ message: "کتاب موردنظر پیدا نشد" }));
+        res.end();
+    }
 }
+
+module.exports = {
+    getAll, getById
+};
--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const fs = require("fs");
 const bookController = require("./controllers/bookController");
 
 const server = http.createServer((req, res) => {
-
     // فعال‌سازی CORS
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
@@ -17,10 +16,21 @@ const server = http.createServer((req, res) => {
         return;
     }
 
-    if (req.method === "GET" && req.url === "/api/books") {
+    // ✅ پردازش URL فقط در یک‌جا
+    const parsedUrl = url.parse(req.url, true);
+    const { pathname, query } = parsedUrl;
+
+    if (req.method === "GET" && pathname === "/api/books" && !query.id) {
         bookController.getAll(req, res);
     }
-
+    else if (req.method === "GET" && pathname === "/api/books" && query.id) {
+        bookController.getById(req, res, query.id);  // ✅ ارسال id مستقیماً به کنترلر
+    }
+    else {
+        res.writeHead(404, { "Content-Type": "application/json" });
+        res.write(JSON.stringify({ message: "مسیر یافت نشد" }));
+        res.end();
+    }
 });
 
 server.listen(3000, () => {
